71a8dfb054db7b8a7d4342a7e45ed36849fae384
kanaarima <137962405+kanaarima@users.noreply.github.com>
2023-08-15 00:45:19 +0200

    Add base_url to API Handler

--- source/utils/api.py
+++ source/utils/api.py
@@ -3,11 +3,12 @@ import time
 
 class ApiHandler:
 
-    def __init__(self, delay=1, headers={'user-agent': 'akatsukialt!/KompirBot fetch service'}):
+    def __init__(self, delay=1, base_url="", headers={'user-agent': 'akatsukialt!/KompirBot fetch service'}):
         self.delay = delay
+        self.base_url = base_url
         self.headers = headers
 
     def get_request(self, URL):
         time.sleep(self.delay)
-        return requests.get(URL, headers=self.headers)
+        return requests.get(f"{self.base_url}{URL}", headers=self.headers)
 
