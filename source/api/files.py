300840f29bd3b9a810190245948032dcb6a28252
kanaarima <137962405+kanaarima@users.noreply.github.com>
2023-08-15 23:03:06 +0200

    Files API

---
+++ source/api/files.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+import json
+import gzip
+import time
+
+
+class DataFile:
+    def __init__(self, filepath) -> None:
+        self.filepath = filepath
+        self.data = None
+
+    def load_data(self, default={}):
+        self.wait_lock()
+        self.lock()
+        try:
+            with gzip.open(self.filepath, "r") as fin:
+                self.data = json.loads(fin.read().decode("utf-8"))
+        except:
+            self.data = default
+        self.unlock()
+
+    def save_data(self):
+        self.wait_lock()
+        if not self.data:
+            self.load_data()
+        self.lock()
+        with gzip.open(self.filepath, "w") as fout:
+            fout.write(json.dumps(self.data).encode("utf-8"))
+        self.unlock()
+
+    def delete(self):
+        Path(self.filepath).unlink(missing_ok=True)
+
+    def wait_lock(self):
+        while Path(self.filepath + ".lock").exists():
+            time.sleep(0.1)
+
+    def lock(self):
+        Path(self.filepath + ".lock").touch(exist_ok=True)
+
+    def unlock(self):
+        Path(self.filepath + ".lock").unlink(missing_ok=True)
