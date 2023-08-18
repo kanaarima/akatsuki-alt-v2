9f7705d8d12c2bc9e9042d62c1969740a8c615ff
kanaarima <137962405+kanaarima@users.noreply.github.com>
2023-08-18 10:00:07 +0200

    Add bit mod parser

--- source/api/utils.py
+++ source/api/utils.py
@@ -34,3 +34,47 @@ def find_unique(check_func, iterA, iterB):
 
 def yesterday() -> datetime.datetime:
     return (datetime.datetime.today() - datetime.timedelta(days=1)).date()
+
+
+NoMod = 0
+NoFail = 1
+Easy = 2
+TouchDevice = 4
+Hidden = 8
+HardRock = 16
+SuddenDeath = 32
+DoubleTime = 64
+Relax = 128
+HalfTime = 256
+Nightcore = 512
+Flashlight = 1024
+SpunOut = 4096
+
+
+def get_mods(magic_number):
+    mods = list()
+    if magic_number & SpunOut:
+        mods.append("SO")
+    if magic_number & Flashlight:
+        mods.append("FL")
+    if magic_number & Nightcore:
+        mods.append("NC")
+    if magic_number & HalfTime:
+        mods.append("HT")
+    if magic_number & Relax:
+        mods.append("RX")
+    if magic_number & DoubleTime:
+        mods.append("DT")
+    if magic_number & SuddenDeath:
+        mods.append("SD")
+    if magic_number & HardRock:
+        mods.append("HR")
+    if magic_number & Hidden:
+        mods.append("HD")
+    if magic_number & TouchDevice:
+        mods.append("TD")
+    if magic_number & Easy:
+        mods.append("EZ")
+    if magic_number & NoFail:
+        mods.append("NF")
+    return mods
