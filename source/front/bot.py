dc97b11aeefa39cebd3b188ed84de9fea3fec6ae
kanaarima <137962405+kanaarima@users.noreply.github.com>
2023-08-18 07:48:47 +0200

    Basic discord bot

--- source/front/bot.py
+++ source/front/bot.py
@@ -1,2 +1,16 @@
+from config import config
+import discord
+
+
+class Bot(discord.Client):
+    async def on_ready(self):
+        print(f"Logged on as {self.user}!")
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = Bot(intents=intents)
+
+
 def main():
-    pass
+    client.run(config["discord"]["bot_token"])
