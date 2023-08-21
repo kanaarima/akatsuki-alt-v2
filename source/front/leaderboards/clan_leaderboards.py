a5e53d4a51b4547dacd16ef3c3bb007e402ea2cf
kanaarima <137962405+kanaarima@users.noreply.github.com>
2023-08-21 03:02:40 +0200

    Post clan leaderboard updates

---
+++ source/front/leaderboards/clan_leaderboards.py
@@ -0,0 +1,94 @@
+from api.objects import Ranking, Player, Clan, GamemodeStatistics, gamemodes
+from typing import Dict, Tuple, List, Optional
+from front.commands import _format_gain_string
+from config import config
+import front.bot as bot
+import discord
+
+
+# Clan, Statistics, Ranking 1s, Ranking pp
+def generate_clan_leaderboards(
+    clan_new: Dict[str, Tuple[Clan, GamemodeStatistics, Ranking, Ranking]],
+    clan_old: Dict[str, Tuple[Clan, GamemodeStatistics, Ranking, Ranking]],
+) -> Dict[str, List[str]]:
+    to_post = {}
+    for name, gamemode in gamemodes.items():
+        clan_pp = dict()
+        clan_1s = dict()
+        for clan, stats, ranking_1s, ranking_pp in clan_new[name]:
+            if ranking_pp["global_ranking"] > 0:
+                clan_pp[clan["clan_id"]] = [(clan, stats, ranking_pp), None]
+            if ranking_1s["global_ranking"] > 0:
+                clan_1s[clan["clan_id"]] = [(clan, stats, ranking_1s), None]
+        for clan, stats, ranking_1s, ranking_pp in clan_old[name]:
+            if clan["clan_id"] in clan_pp:
+                clan_pp[clan["clan_id"]][1] = (clan, stats, ranking_pp)
+            if clan["clan_id"] in clan_1s:
+                clan_1s[clan["clan_id"]][1] = (clan, stats, ranking_1s)
+        to_post[f"clan_{name}_pp"] = generate_list(
+            list(clan_pp.values()), format_clan_pp
+        )
+        to_post[f"clan_{name}_1s"] = generate_list(
+            list(clan_1s.values()), format_clan_1s
+        )
+    return to_post
+
+
+def generate_list(
+    data: List[
+        Tuple[
+            Tuple[Clan, GamemodeStatistics, Ranking],
+            Optional[Tuple[Clan, GamemodeStatistics, Ranking]],
+        ]
+    ],
+    format_func,
+) -> List[str]:
+    str_list = list()
+    for new, old in sorted(data, key=lambda x: x[0][2]["global_ranking"]):
+        str_list.append(format_func(new, old))
+    return str_list
+
+
+def format_clan_pp(
+    data_new: Tuple[Clan, GamemodeStatistics, Ranking],
+    data_old: Tuple[Clan, GamemodeStatistics, Ranking],
+):
+    pp_gain = ""
+    rank_gain = ""
+    if data_old:
+        rank = _format_gain_string(
+            gain=data_new[2]["global_ranking"] - data_old[2]["global_ranking"],
+        )
+        pp = ""
+        if "total_pp" in data_old[1]:
+            pp = _format_gain_string(
+                gain=data_new[1]["total_pp"] - data_old[1]["total_pp"],
+                fix="pp",
+            )
+        rank_gain = f" {rank}"
+        pp_gain = f" {pp}"
+    tag = ""
+    if "clan_tag" in data_new[0] and data_new[0]["clan_tag"]:
+        tag = f" [{data_new[0]['clan_tag']}]"
+    return f"#{data_new[2]['global_ranking']} {data_new[0]['clan_name']}{tag}{rank_gain} Performance points: {data_new[1]['total_pp']}pp{pp_gain}"
+
+
+def format_clan_1s(
+    data_new: Tuple[Clan, GamemodeStatistics, Ranking],
+    data_old: Tuple[Clan, GamemodeStatistics, Ranking],
+):
+    fp_gain = ""
+    rank_gain = ""
+    if data_old:
+        rank = _format_gain_string(
+            gain=data_new[2]["global_ranking"] - data_old[2]["global_ranking"],
+        )
+        fp = ""
+        if "total_1s" in data_old[1]:
+            fp = _format_gain_string(
+                gain=data_new[1]["total_1s"] - data_old[1]["total_1s"]
+            )
+        rank_gain = f" {rank}"
+        fp_gain = f" {fp}"
+    tag = f" [{data_new[0]['clan_tag']}]"
+    return f"#{data_new[2]['global_ranking']} {data_new[0]['clan_name']}{tag}{rank_gain} First places: {data_new[1]['total_1s']}{fp_gain}"
--- source/front/tasks.py
+++ source/front/tasks.py
@@ -1,11 +1,68 @@
+from api.utils import str_to_datetime, datetime_to_str, yesterday, other_yesterday
+from front.leaderboards import clan_leaderboards
+from api.files import DataFile
+from datetime import datetime
 from discord.ext import tasks
-import front.bot as bot
-import discord
+from config import config
+from front import bot
+import asyncio
 
 
-@tasks.loop(seconds=60)
-async def post_lb_updates():
+async def post_list(channel_id, strings):
+    str = "```"
+    for string in strings:
+        str += f"{string}\n"
+        if len(str) > 1900:
+            str += "```"
+            await bot.client.get_channel(channel_id).send(content=str)
+            await asyncio.sleep(1)
+            str = "```"
+    str += "```"
+    if str != "```":
+        await bot.client.get_channel(channel_id).send(content=str)
+    await asyncio.sleep(1)
+
+
+async def post_clan_updates():
+    clan_file = DataFile(
+        f"{config['common']['data_directory']}/leaderboards/clans/{yesterday()}.json.gz"
+    )
+    clan_old_file = DataFile(
+        f"{config['common']['data_directory']}/leaderboards/clans/{other_yesterday()}.json.gz"
+    )
+    if not await clan_file.wait_till_exist(timeout=180):
+        return
+    if not clan_old_file.exists():
+        clan_old_file = clan_file
+    clan_file.load_data()
+    clan_old_file.load_data()
+    lists = clan_leaderboards.generate_clan_leaderboards(
+        clan_new=clan_file.data, clan_old=clan_old_file.data
+    )
+    for key in lists.keys():
+        if key not in config["discord"]["channels"]:
+            print(f"{key} not found!")
+            continue
+        await post_list(config["discord"]["channels"][key], lists[key])
+
+
+async def post_user_updates():
     pass
+
+
+@tasks.loop(minutes=10)
+async def post_lb_updates():
+    file = DataFile(f"{config['common']['data_directory']}/poster.json.gz")
+    file.load_data(
+        default={"last_posted": datetime_to_str(datetime(year=2000, month=1, day=1))}
+    )
+    date = str_to_datetime(file.data["last_posted"])
+    if date.day == datetime.now().day:
+        return
+    file.data["last_posted"] = datetime_to_str(datetime.now())
+    file.save_data()
+    await post_clan_updates()
+    await post_user_updates()
 
 
 def init_tasks():
