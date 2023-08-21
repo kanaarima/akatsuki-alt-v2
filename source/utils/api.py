52acb67855067e9a802e1f341c02fc3617b20cf8
kanaarima <137962405+kanaarima@users.noreply.github.com>
2023-08-21 17:44:56 +0200

    Expose headers

--- source/utils/api.py
+++ source/utils/api.py
@@ -1,9 +1,11 @@
 import requests
 import time
 
-class ApiHandler:
+DEFAULT_HEADERS = {"user-agent": "akatsukialt!/KompirBot fetch service"}
 
-    def __init__(self, delay=1, base_url="", headers={'user-agent': 'akatsukialt!/KompirBot fetch service'}):
+
+class ApiHandler:
+    def __init__(self, delay=1, base_url="", headers=DEFAULT_HEADERS):
         self.delay = delay
         self.base_url = base_url
         self.headers = headers
@@ -11,4 +13,3 @@ class ApiHandler:
     def get_request(self, URL):
         time.sleep(self.delay)
         return requests.get(f"{self.base_url}{URL}", headers=self.headers)
-
