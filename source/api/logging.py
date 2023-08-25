f7db942a111b15ef655ec22874d7bc825d67cfef
kanaarima <137962405+kanaarima@users.noreply.github.com>
2023-08-25 18:00:22 +0200

    Fix logger

--- source/api/logging.py
+++ source/api/logging.py
@@ -17,22 +17,18 @@ class GZipRotator:
         os.remove(dest)
 
 
-logger: logging.Logger = None
-
-
-def init(type):
-    logformatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s")
-    log = logging.handlers.TimedRotatingFileHandler(
-        f"{config['common']['log_directory']}/debug_{type}.log",
-        "midnight",
-        1,
-        backupCount=5,
-    )
-    log.setLevel(logging.DEBUG)
-    log.setFormatter(logformatter)
-    log.rotator = GZipRotator()
-
-    logger = logging.getLogger("main")
-    logger.addHandler(log)
-    logger.addHandler(logging.StreamHandler(sys.stdout))
-    logger.setLevel(logging.DEBUG)
+type = "frontend" if "frontend" in "".join(sys.argv) else "backend"
+logformatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s")
+log = logging.handlers.TimedRotatingFileHandler(
+    f"{config['common']['log_directory']}/debug_{type}.log",
+    "midnight",
+    1,
+    backupCount=5,
+)
+log.setLevel(logging.DEBUG)
+log.setFormatter(logformatter)
+log.rotator = GZipRotator()
+logger = logging.getLogger("main")
+logger.addHandler(log)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+logger.setLevel(logging.DEBUG)
