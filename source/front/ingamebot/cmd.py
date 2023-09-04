8541ad65dccc01ee1763762c71e46a40c3d007e6
Lekuru <contact@lekuru.xyz>
2023-09-04 23:11:23 +0200

    Cleanup in-game commands

--- source/front/ingamebot/cmd.py
+++ source/front/ingamebot/cmd.py
@@ -13,38 +13,56 @@ def ping(player: Player, message, args):
 
 
 def recommend(player: Player, message, args):
+    # Fetch user stats
     _, stats = akatsuki.get_user_stats(player.id, no_1s=True)
     top100 = akatsuki.get_user_best(player.id, gamemodes["std_rx"])
+
+    # Exclude top play
+    # TODO: Exclude all plays that are already fced?
     skip_id = [play["beatmap_id"] for play in top100[0]]
+
+    # Calculate min/max pp
     total_pp = stats["std_rx"][0]["total_pp"]
     target = total_pp / 20
     min_pp = target - 20
     max_pp = target + 20
+
     mods_include = []
     mods_exclude = ["EZ", "FL"]
+
+    # Parse command arguments
     parsed = _parse_args(args, nodefault=True)
+
     if "min_pp" in parsed:
         if not parsed["min_pp"].isnumeric():
             player.send_message("pp value should be a number.")
             return
         min_pp = int(parsed["min_pp"])
         max_pp = min_pp + 20
+
     if "max_pp" in parsed:
         if not parsed["max_pp"].isnumeric():
             player.send_message("pp value should be a number.")
             return
         max_pp = int(parsed["max_pp"])
-    mods = parsed["mods"].upper() if "mods" in parsed else None
+
     if "include_mods" in parsed:
         mods_include = [
             parsed["include_mods"][i : i + 2]
             for i in range(0, len(parsed["include_mods"]), 2)
         ]
+
     if "exclude_mods" in parsed:
         mods_exclude = [
             parsed["exclude_mods"][i : i + 2]
             for i in range(0, len(parsed["exclude_mods"]), 2)
         ]
+
+    mods = (
+        parsed["mods"].upper()
+        if "mods" in parsed else None
+    )
+
     recommend = farmer.recommend(
         pp_min=min_pp,
         pp_max=max_pp,
@@ -53,9 +71,14 @@ def recommend(player: Player, message, args):
         mods_exclude=mods_exclude,
         skip_id=skip_id,
     )
-    beatmap = beatmaps.load_beatmap(recommend[0]["beatmap_id"])
-    link = f"osu://b/{beatmap['beatmap_id']}"
+
+    if not (beatmap := beatmaps.load_beatmap(recommend[0]["beatmap_id"])):
+        player.send_message('Failed to load beatmap.')
+        return
+
     title = f"{beatmap['title']} [{beatmap['difficulty_name']}] +{recommend[0]['mods']} {int(recommend[0]['average_pp'])}pp (confidence: {recommend[0]['weight']*100:.2f}%)"
+    link = f"osu://b/{beatmap['beatmap_id']}"
+
     player.send_message(f"[{link} {title}]")
 
 
@@ -64,11 +87,13 @@ def recommend_score(player: Player, message, args):
     scores = DataFile(
         f"{config['common']['data_directory']}/users_statistics/scores/{player.id}.json.gz"
     )
+
     if scores.exists():
         scores.load_data()
         skip_id.extend(
             int(beatmapid) for beatmapid in list(scores.data["std_rx"].keys())
         )
+
     beatmap = farmer.recommend_score(skip_id, 1)[0]
     link = f"osu://b/{beatmap['beatmap_id']}"
     title = f"{beatmap['title']} [{beatmap['difficulty_name']}] {int(beatmap['max_score']):,} (score/minute: {int(beatmap['score_minute']):,})"
@@ -76,9 +101,13 @@ def recommend_score(player: Player, message, args):
 
 
 def help(player: Player, message, args):
-    player.send_message("\n".join([
-        "KompirBot made by [https://akatsuki.gg/u/91076?mode=0&rx=1 Adachi].",
-        "Commands:"
-        "!recommend (min_pp=pp) (max_pp=pp) (mods=mods) (include_mods=mods) (exclude_mods=mods) | recommends a farm map",
-        "!recommend_score | recommends a score farm map"
-    ]))
+    player.send_message(
+        "\n".join(
+            [
+                "KompirBot made by [https://akatsuki.gg/u/91076?mode=0&rx=1 Adachi].",
+                "Commands:",
+                "   !recommend <min_pp=pp> <max_pp=pp> <mods=mods> <include_mods=mods> <exclude_mods=mods> - Recommends a farm map",
+                "   !recommend_score - Recommends a score farm map",
+            ]
+        )
+    )
