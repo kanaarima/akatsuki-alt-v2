9fa1a080e9fb709f28defb91f22ecb9762e650d3
Levi <contact@lekuru.xyz>
2023-09-04 09:17:59 +0200

    Add experimental color logging

--- source/api/logging.py
+++ source/api/logging.py
@@ -5,6 +5,30 @@ import gzip
 import sys
 import os
 
+class ColorFormatter(logging.Formatter):
+
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    cyan = "\x1b[96m"
+    reset = "\x1b[0m"
+
+    format_prefix = '[%(asctime)s] - <%(name)s> '
+    format = '%(levelname)s: %(message)s'
+
+    FORMATS = {
+        logging.DEBUG:    grey + format_prefix            + format + reset,
+        logging.INFO:     grey + format_prefix + cyan     + format + reset,
+        logging.WARNING:  grey + format_prefix + yellow   + format + reset,
+        logging.ERROR:    grey + format_prefix + red      + format + reset,
+        logging.CRITICAL: grey + format_prefix + bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
 
 class GZipRotator:
     def __call__(self, source, dest):
@@ -26,7 +50,9 @@ file_handler = logging.handlers.TimedRotatingFileHandler(
     backupCount=5,
 )
 file_handler.rotator = GZipRotator()
+
 console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setFormatter(ColorFormatter())
 
 logging.basicConfig(
     level=logging.INFO,
