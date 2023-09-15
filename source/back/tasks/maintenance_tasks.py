53c573e7c5e7afb502e837cf2ad72b3b851eef1a
kanaarima <137962405+kanaarima@users.noreply.github.com>
2023-09-15 07:27:12 +0200

    crawl entire mapsets once

--- source/back/tasks/maintenance_tasks.py
+++ source/back/tasks/maintenance_tasks.py
@@ -4,6 +4,7 @@ from api.files import DataFile, BinaryFile, exists
 from datetime import datetime, timedelta
 from api.tasks import Task, TaskStatus
 from api.objects import gamemodes
+import api.database as database
 import api.beatmaps as beatmaps
 import api.akatsuki as akatsuki
 from api.logging import get_logger
@@ -20,31 +21,46 @@ logger = get_logger("tasks.maintenance")
 class CheckNewRankedBeatmaps(Task):
     def __init__(self) -> None:
         super().__init__(asynchronous=True)
-        self.last = datetime(year=1984, month=1, day=1)
 
     def can_run(self) -> bool:
-        return (datetime.now() - self.last) > timedelta(days=1)
+        return (
+            datetime.now()
+            - datetime.fromtimestamp(database.get_task("checknewrankedbeatmaps"))
+        ) > timedelta(days=1)
 
     def run(self) -> TaskStatus:
-        self.last = datetime.now()
+        iterate_all = database.get_task("checknewrankedbeatmaps") == 0
         sets = beatmaps.client.search_beatmapsets(
             mode=0,
             sort=BeatmapsetSearchSort.RANKED_DESCENDING,
             category=BeatmapsetSearchCategory.HAS_LEADERBOARD,
         )
-        for set in sets.beatmapsets:
-            for beatmap in set.beatmaps:
-                beatmap._beatmapset = set
-                if self.suspended:
-                    return TaskStatus.SUSPENDED
-                if not exists(f"{beatmaps.base_path}/{beatmap.id}.json.gz"):
-                    logger.info(f"Found new ranked beatmap {beatmap.id}")
-                    beatmaps.save_beatmap(
-                        {"beatmap_id": beatmap.id, "raw_beatmap": beatmap}
-                    )
+        while True:
+            for set in sets.beatmapsets:
+                for beatmap in set.beatmaps:
+                    beatmap._beatmapset = set
+                    if self.suspended:
+                        return TaskStatus.SUSPENDED
+                    if not exists(f"{beatmaps.base_path}/{beatmap.id}.json.gz"):
+                        logger.info(f"Found new ranked beatmap {beatmap.id}")
+                        beatmaps.save_beatmap(
+                            {"beatmap_id": beatmap.id, "raw_beatmap": beatmap}
+                        )
+            if not iterate_all:
+                break
+            sets = beatmaps.client.search_beatmapsets(
+                mode=0,
+                sort=BeatmapsetSearchSort.RANKED_DESCENDING,
+                category=BeatmapsetSearchCategory.HAS_LEADERBOARD,
+                cursor=sets.cursor,
+            )
+            if not sets:
+                break
+        database.set_task("checknewrankedbeatmaps", time.time())
         return self._finish()
 
 
+# TODO remove
 class BuildBeatmapCache(Task):
     def __init__(self) -> None:
         super().__init__(asynchronous=True)
